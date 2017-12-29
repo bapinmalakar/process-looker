4aa412dd28601bf3180aa0010b0228b858573874
biplab malakar <biplab.x@venturepact.com>
2017-12-29 15:51:46 +0530

    chnages

--- app/controller/strater.js
+++ app/controller/strater.js
@@ -5,13 +5,12 @@ remote.app.getAppPath();
 const path = require("path");
 const processHelper = require(path.resolve("app/script/process.js"));
 let processDetails = [];
+let snapTime, processNumber;
 
 $(document).ready(() => {
-  alert("Script loade");
   $("#progressDiv").hide();
 });
 $("#snapBtn").on("click", async () => {
-  alert("btn clicked");
   $("#snapBtn").text("Wait........");
   $("#snapBtn").prop("disabled", true);
   try {
@@ -30,14 +29,34 @@ $("#snapBtn").on("click", async () => {
       i++;
     }
     processDetails = processHelper.filterProcessData(processDetails);
+    prepareList();
     $("#snapBtn").text("Take Current Processes Snapshot");
     $("#snapBtn").prop("disabled", false);
   } catch (err) {
-    alert("Some error occured, try again!" + err);
+    alert("Some error occured, try again!");
     $("#snapBtn").text("Take Current Processes Snapshot");
     $("#snapBtn").prop("disabled", false);
   }
 });
+function prepareList() {
+  $("#snapInfo").text(
+    `SnapTime:${snapTime} Number Of Process: ${processNumber}     Different type of Process: ${processDetails.length}`
+  );
+  let color = ['#00008b', "white"];
+  let i = 0;
+  $("ul").empty();
+  let ul = $("ul");
+  for (let item of processDetails) {
+    ul.append(
+      `<li id="list-${i}" title="${item.count} of process ${
+        item.name
+      }"><b><span style="color: ${color[i % 2]}; cursor: pointer;">${item.name}     ${
+        item.count
+      }</span></b></li>`
+    );
+    i++;
+  }
+}
 async function getProcessList() {
   try {
     let processes = await processHelper.processFind();
@@ -48,6 +67,8 @@ async function getProcessList() {
       date.getMinutes().toString() +
       ":" +
       date.getSeconds().toString();
+    snapTime = formateDate;
+    processNumber = processes.length;
     $("#snapInfo").text(
       `SnapTime:${formateDate} Number Of Process: ${processes.length}`
     );
